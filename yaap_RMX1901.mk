42e814e6b62ca02d3ab4f839b3c7a0876317fce4
sksayz5 <137992110+sksayz5@users.noreply.github.com>
2024-09-08 08:22:55 +0530

    Update and rename lineage_RMX1901.mk to yaap_RMX1901.mk

--- yaap_RMX1901.mk
+++ yaap_RMX1901.mk
@@ -8,12 +8,11 @@
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_HAS_UDFPS := true
 TARGET_SUPPORTS_QUICK_TAP := true
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_USES_MINI_GAPPS:= true
+TARGET_GAPPS_ARCH := arm64
 BUILD_SEEDVAULT := true
 TARGET_DISABLE_EPPE := true
 
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
 
 
 # Inherit from RMX1901 device
@@ -26,7 +25,7 @@ $(call inherit-product, packages/apps/ViPER4AndroidFX/config.mk)
 PRODUCT_BRAND := realme
 PRODUCT_DEVICE := RMX1901
 PRODUCT_MANUFACTURER := realme
-PRODUCT_NAME := lineage_RMX1901
+PRODUCT_NAME := yaap_RMX1901
 PRODUCT_MODEL := RMX1901
 
 PRODUCT_SYSTEM_NAME := RMX1901
