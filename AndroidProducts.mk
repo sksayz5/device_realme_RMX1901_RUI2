ddd0f8e3b18d52ce76b9701d65099b260ed24aff
sksayz5 <137992110+sksayz5@users.noreply.github.com>
2024-09-10 17:04:24 +0530

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,8 +5,8 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_RMX1901.mk
+    $(LOCAL_DIR)/blaze_RMX1901.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_RMX1901-userdebug \
-    aosp_RMX1901-eng
+    blaze_RMX1901-userdebug \
+    blaze_RMX1901-eng
